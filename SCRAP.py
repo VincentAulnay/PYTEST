07ff41b6539487a8f9b77bec2b8678226413aa08
VincentAulnay <48682002+VincentAulnay@users.noreply.github.com>
2020-07-16 14:13:11 +0200

    Update SCRAP.py

--- SCRAP.py
+++ SCRAP.py
@@ -21,6 +21,8 @@ import sys
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 #prefs = {"profile.managed_default_content_settings.images": 2}
 #chrome_options.add_experimental_option("prefs", prefs)
 
