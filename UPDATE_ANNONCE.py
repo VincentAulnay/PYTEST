e9b9aaf36cebc91fc275aff51073e6ee8ee0f681
VincentAulnay <48682002+VincentAulnay@users.noreply.github.com>
2020-11-02 17:17:49 +0100

    Update UPDATE_ANNONCE.py

--- UPDATE_ANNONCE.py
+++ UPDATE_ANNONCE.py
@@ -655,16 +655,16 @@ while c<=nrow:
 				ele=driver.find_element_by_xpath("//div[@class='_cg8a3u']")
 				driver.execute_script("arguments[0].scrollIntoView(true);", ele)
 				#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-				time.sleep(2)
+				time.sleep(3)
 				driver.execute_script("window.scrollBy(0,-800);")
 				#driver.execute_script("window.scrollBy(0,500);")
 				f_ele=6
-				time.sleep(2)
+				time.sleep(1)
 			except:
 				#driver.execute_script("window.scrollBy(0,1000);")
 				f_ele=f_ele+1
 				time.sleep(2)
-		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+		#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 	#PROFILE
 		html = driver.page_source
 		time.sleep(2)
