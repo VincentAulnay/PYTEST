e33227097692281bfd3706519690923d19aecb90
VincentAulnay <48682002+VincentAulnay@users.noreply.github.com>
2021-07-13 15:27:06 +0200

    Update CLEAN.py

--- CLEAN.py
+++ CLEAN.py
@@ -67,7 +67,7 @@ def Clean(testmois):
 			i=0
 			while i<len(vlist):
 				#if '20-6' not in vlist[i] and '21-6' not in vlist[i] and'22-6' not in vlist[i] and '23-6' not in vlist[i] and '24-6' not in vlist[i] and '25-6' not in vlist[i] and '26-6' not in vlist[i] and '30-6' not in vlist[i]:
-				if '12-07' not in vlist[i]:
+				if 'R12-07' not in vlist[i]:
 					if newlist=='':
 						newlist=newlist+vlist[i]
 					else:
