8978fc450701c352c58334c599a9ee6134e7a726
VincentAulnay <48682002+VincentAulnay@users.noreply.github.com>
2022-09-26 12:04:45 +0200

    Update TEST_scrap.py

--- TEST_scrap.py
+++ TEST_scrap.py
@@ -27,7 +27,7 @@ chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.managed_default_content_settings.images": 2}
 chrome_options.add_experimental_option("prefs", prefs)
 chrome_options.add_argument("window-size=2000,1000")
-#chrome_options.add_argument("-headless")
+chrome_options.add_argument("-headless")
 #chrome_options.add_argument("-disable-gpu")
 print ('▀▄▀▄▀▄ STOPBNB ▄▀▄▀▄▀')
 
