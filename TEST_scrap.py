727830febff8a3ceaa41821e245e15d0b77f38ba
VincentAulnay <48682002+VincentAulnay@users.noreply.github.com>
2022-09-26 12:56:19 +0200

    Update TEST_scrap.py

--- TEST_scrap.py
+++ TEST_scrap.py
@@ -33,6 +33,8 @@ print ('▀▄▀▄▀▄ STOPBNB ▄▀▄▀▄▀')
 
 rootdriver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver',chrome_options=chrome_options)
 time.sleep(5)
+rootdriver.set_window_position(500, 50)
+time.sleep(5)
 rootdriver.set_window_size(2000, 1000)
 time.sleep(5)
 rootdriver.get('https://www.airbnb.fr/rooms/718608557683071399')
